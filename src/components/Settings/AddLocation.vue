05b67a20a0712e1e6069a933e3af2473119d2a3f
Maksim Smetanin <maks.s923@mail.ru>
2022-09-08 18:55:50 +0500

    fix: move svg in single component

--- src/components/Settings/AddLocation.vue
+++ src/components/Settings/AddLocation.vue
@@ -3,44 +3,10 @@
     <label class="add-location">
       <span class="add-location__title">Add Location:</span>
       <div class="add-location__inner">
-        <AppInput v-model="location" @keyup.enter="handleAddLocation" v-focus/>
-        <svg
-          @click="handleAddLocation"
-          class="add-location__icon"
-          version="1.1"
-          id="Layer_1"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          viewBox="0 0 511.999 511.999"
-          style="enable-background: new 0 0 511.999 511.999"
-          xml:space="preserve"
-        >
-          <g>
-            <g>
-              <polygon
-                points="511.999,145.102 470.412,145.102 470.412,235.206 79.606,235.206 200.941,113.871 171.535,84.465 0,255.999 
-			171.535,427.533 200.941,398.128 79.606,276.792 511.999,276.792 		"
-              />
-            </g>
-          </g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-        </svg>
+        <AppInput v-model="location" @keyup.enter="handleAddLocation" v-focus />
+        <span @click="handleAddLocation">
+          <SvgAddLocation />
+        </span>
       </div>
     </label>
   </form>
@@ -51,23 +17,24 @@ import { ERROR_KEY } from "@/injectKeys";
 import { inject, ref, watch } from "vue";
 import { focus as vFocus } from "@/directives/focus";
 import AppInput from "../UI/AppInput.vue";
+import SvgAddLocation from "../Svg/SvgAddLocation.vue";
 
 const emit = defineEmits<{
-  (e: 'add-location', value: string): void,
-}>()
+  (e: "add-location", value: string): void;
+}>();
 
-const location = ref('')
+const location = ref("");
 const handleAddLocation = () => {
-  if(location.value) {
-    emit('add-location', location.value)
-    location.value = ''
+  if (location.value) {
+    emit("add-location", location.value);
+    location.value = "";
   }
-}
+};
 
-const injections = inject(ERROR_KEY)
+const injections = inject(ERROR_KEY);
 watch(location, () => {
-  if(location.value && injections?.error) {
-    injections.resetError()
+  if (location.value && injections?.error) {
+    injections.resetError();
   }
-})
+});
 </script>
